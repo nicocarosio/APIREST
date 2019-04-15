74e11f4d6b5c0d2d252d0b12890e3058634e74a1
Nico Carosio <nicocarosio@gmail.com>
2019-04-15 09:36:20 +0200

    implementacion de un ENDPOINT  para el método POST de HTTP. Pruebas con POSTMAN

--- index.js
+++ index.js
@@ -1,19 +1,40 @@
-'use strict';
+'use strict'
 
-//importaciones
+//importations
 const express = require('express')
 const bodyParser = require('body-parser')
 
 //variables
 const app = express()
-const port = process.env.PORT || 3002
+const port = process.env.PORT || 3000
 
 //middlewares
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 
-//crear un servidor
-app.listen(3000, ()=>{
+app.get('/api/product', (req, res)=>{
+res.send(200, {products:[] })
+})
+
+app.get('/api/product/:productId', (req, res)=>{
+
+})
+
+app.post('/api/product', (req, res)=>{
+    console.log(req.body)
+    res.status(404).send({message: 'El producto no existe....se ha recibido correctamente'})
+    
+})
+
+ app.put('/api/product/:productId', (req, res)=>{
+
+ })
+
+ app.delete('/api/product/:productId', (req, res)=>{
+
+ })
+
+app.listen(port, ()=>{
 console.log(`API REST corriendo en http://localhost:${port}`)
 })
