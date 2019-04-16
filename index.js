63290c46224304c653a3b1902555769c0df03073
Nico Carosio <nicocarosio@gmail.com>
2019-04-16 12:29:09 +0200

    instalar MongoDB y utilizar Mongoose como ORM/ODM

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 //importations
 const express = require('express')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 
 //variables
 const app = express()
@@ -35,6 +36,15 @@ app.post('/api/product', (req, res)=>{
 
  })
 
-app.listen(port, ()=>{
-console.log(`API REST corriendo en http://localhost:${port}`)
-})
+ mongoose.connect('mongodb://localhost:27017/shop', { useNewUrlParser: true }, (err,res)=>{
+     if(err) {
+         return console.log(`Error al conectarse a la base de datos: ${err}`)
+     }
+     console.log('Conexión a la base de datos establecida...')
+     
+     app.listen(port, ()=>{
+        console.log(`API REST corriendo en http://localhost:${port}`)
+        })     
+
+ })
+
